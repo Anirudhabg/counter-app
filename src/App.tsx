63208e0458d7f74baf8241a099bc2641bd2cec98
Anirudhabg <anirudhabg@gmail.com>
2025-02-18 14:22:47 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,16 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrement
+        </button>
+        <button onClick={() => setCount((count) => count = 0)}>
+          Reset
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          Count is {count}        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
